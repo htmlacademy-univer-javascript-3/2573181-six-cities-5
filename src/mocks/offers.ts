bc741ae4f6c3e71a6e6e19dbc6a374315cd96c69
mnogodumal <raskol@duck.com>
2024-11-13 22:30:00 +0300

    module4-task1

---
+++ src/mocks/offers.ts
@@ -0,0 +1,30 @@
+export const DATA = [
+  {
+    id: 1,
+    img: '',
+    price: '120',
+    name: 'Beautiful & luxurious apartment at great location',
+    reviews: 4,
+  },
+  {
+    id: 2,
+    img: '',
+    price: '100',
+    name: 'Beautiful & luxurious apartment at great location',
+    reviews: 4,
+  },
+  {
+    id: 3,
+    img: '',
+    price: '90',
+    name: 'Murino',
+    reviews: 4,
+  },
+  {
+    id: 4,
+    img: '',
+    price: '120',
+    name: 'Beautiful & luxurious apartment at great location',
+    reviews: 4,
+  },
+];
